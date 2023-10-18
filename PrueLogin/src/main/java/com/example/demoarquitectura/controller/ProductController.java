0930fd4ace5d9183d1d8d9f9025092533a36faab
Santiago Mora <111421466+SantiagoMoraV@users.noreply.github.com>
2023-10-18 11:27:45 -0500

    Programa con Login

---
+++ PrueLogin/src/main/java/com/example/demoarquitectura/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.example.demoarquitectura.controller;
+
+import com.example.demoarquitectura.entity.Product;
+import com.example.demoarquitectura.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hola Mundo!!";
+    }
+
+    @PostMapping("/save")
+    public Product save(@RequestBody Product p){
+        return productService.save(p);
+    }
+
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Product getById(@PathVariable("id") Integer id){
+        return productService.getById(id);
+    }
+
+}
