bdf684eb53640c70a0dc8152e537ae5f6d7f7a9f
chechitox11 <89945478+chechitox11@users.noreply.github.com>
2023-11-24 13:21:09 -0500

    Delete Electronica - proyecto directory

--- Electronica - proyecto/src/main/java/com/example/demoarquitectura/repository/crud/QRCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoarquitectura.repository.crud;
-
-import com.example.demoarquitectura.entity.QR;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QRCrudRepository extends CrudRepository<QR, Integer> {
-}
